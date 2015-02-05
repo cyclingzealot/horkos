d9846b299422861304ab6725a5ca1b834e822737
Julien Lamarche <julien.lamarche@gmail.com>
2015-02-05 16:04:45 -0500

    added bundle configuration in core symfony kernel and routing files

--- symfony/app/AppKernel.php
+++ symfony/app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Jlam\Cdn2015Bundle\JlamCdn2015Bundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
