215ea3c59586700a0285cb8ac34ad9c64b371202
Julien Lamarche <jlam@credil.org>
2015-02-09 15:26:34 -0500

    updated error messages with more configuration hints

--- symfony/src/Jlam/Cdn2015Bundle/Tests/Bash/test01DynamicPage.bash
+++ symfony/src/Jlam/Cdn2015Bundle/Tests/Bash/test01DynamicPage.bash
@@ -19,8 +19,11 @@ content=`curl --silent $url `
 needle="Hello Fair Vote Canada!"
 
 if [[ ! "$content" =~ "$needle" ]]; then
-  echo "Can't find \"$needle\" at \"$url\" .  The content was \n\n" $content  "\n\n"
+  echo "Can't find \"$needle\" at \"$url\" .  The content was \n\n" $content  "\n\n" | tail
+  echo
   echo See http://symfony.com/doc/current/cookbook/configuration/web_server_configuration.html to configure apache
+  echo Virtual host configration example in doc/cdn2015.conf
+  echo For cache and logs permission settings, see http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
   exit 1
 fi
 
