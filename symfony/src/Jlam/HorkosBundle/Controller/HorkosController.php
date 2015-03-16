779f758705f15114091b06d66dce9ff6ee4adb63
Julien Lamarche <julien.lamarche@gmail.com>
2015-03-16 19:59:57 -0400

    started adding switch to ab2015 and figuring out how to get riding list - WIP

--- symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
+++ symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
@@ -9,6 +9,10 @@ use Jlam\HorkosBundle\Twig\SafeDivideExtension;
 
 class HorkosController extends Controller
 {
+	
+	const BASE_DIR_SCRAPPERS	= 'Jlam\HorkosBundle\\';
+	const DEFAULT_ELECTION		= 'cdn2015';
+	
     public function indexAction()
     {
 		#If a cache is available, use it.
@@ -17,7 +21,8 @@ class HorkosController extends Controller
     	
     	
     	# Get the engine class name
-    	$engineClassName = "Jlam\HorkosBundle\Cdn2015scrapper";
+    	$election			= $this->getRequest()->get('election');
+    	$engineClassName	= self::getScrappingEngineClassName($election);
 
     	# Set the logger for the riding entity
     	Riding::setLogger($this->get('logger'));
@@ -25,7 +30,6 @@ class HorkosController extends Controller
 		#If not, slurp data
 		$engineClassName::initialize($this->container);
 		$engineClassName::scrape();
-		
 		
 		
 		### Prepare the rendering #####################################################
@@ -50,5 +54,20 @@ class HorkosController extends Controller
         
 		#Return the controller 
         return $response;
+    }
+    
+    
+    private static function getScrappingEngineClassName($electionShorthand = null) {
+    	$engineClassNames = array(
+    		'cdn2015'	=> 'Cdn2015scrapper',
+    		'ab2015'	=> 'Ab2015scrapper',
+    	);
+    	
+    	if(!isset($engineClassNames[$electionShorthand])) 
+    		$electionShorthand = self::DEFAULT_ELECTION;
+    	
+    	$engineClassName =  $engineClassNames[$electionShorthand];
+    	
+    	return self::BASE_DIR_SCRAPPERS . $engineClassName;
     }
 }
