5c3e831ff7da8cf3df241a4a0339a34d4face1dc
Julien Lamarche <julien.lamarche@gmail.com>
2015-10-13 22:36:33 -0400

    WIP added csv view

--- symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
+++ symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
@@ -12,30 +12,33 @@ use Jlam\HorkosBundle\phpFastCache;
 
 class HorkosController extends Controller
 {
-	
+
 	const BASE_DIR_SCRAPPERS	= 'Jlam\HorkosBundle\\';
 	const DEFAULT_ELECTION		= 'cdn2015';
 	const CACHE_TTL_SECS		= 30;
-	
+
     public function indexAction()
     {
-    	
+
     	#Get request parameters
-    	$election			= $this->getRequest()->get('election');
+    	$election			= $this->getRequest()->get('election')	?: self::DEFAULT_ELECTION;
     	$language			= 'en';
     	$fresh				= $this->getRequest()->get('fresh');
-    	
+    	$format				= $this->getRequest()->get('format')	?: 'html';
+
+
+
 		#Setup caching
-    	$cacheKey			= "$election.$language";
+    	$cacheKey			= "$election.$language.$format";
     	$root = $this->get('kernel')->getRootDir();
     	require_once("$root/../src/Jlam/HorkosBundle/phpfastcache-final/phpfastcache.php");
     	phpFastCache::setup("storage", "auto");
     	$cache = new phpFastCache();
-    	
-    	
+
+
     	#If a cache is available, use it and return it right away.
     	$response = $cache->get($cacheKey);
-    	
+
     	if($response !== null && $fresh !== 'yes') {
     		return $response;
     	}
@@ -45,26 +48,26 @@ class HorkosController extends Controller
     		return $response;
     	}
     	*/
-    	
-    	
-    	
+
+
+
     	# Get the engine class name
     	$engineClassName	= self::getScrappingEngineClassName($election);
 
     	# Set the logger for the riding entity
     	Riding::setLogger($this->get('logger'));
-    	
+
 		#If not, slurp data
 		$engineClassName::initialize($this->container);
 		$engineClassName::scrape();
 		$engineClassName::validate();
 
-		
-		
+
+
 		### Prepare the rendering #####################################################
 		$summary						= $engineClassName::getSummary();
 		$magnitude = 1;
-		
+
 		if(! $engineClassName::getScraperError()) {
 			$partyTally			= Riding::getPartyTally(FALSE)->getTally();
 			$jurisdictionTally	= Riding::getJurisdictionTally()->getTally();
@@ -78,49 +81,49 @@ class HorkosController extends Controller
 			$partyTallyWasted	= array();
 			$summary['totalWastedVotes']	= null;
 		}
-	
-	    #Render
-        $response = $this->render('JlamHorkosBundle:Horkos:index.html.twig', array(
-        	'ridings'	=> Riding::getAllRdingsSorted(),
-        	'partyTally' 	=> $partyTally,
-        	'jurisdiction'	=> $jurisdictionTally,
-        	'summary'	=> $summary,
-        	'election'	=> $election ? $election : self::DEFAULT_ELECTION,
-        	'error'		=> $engineClassName::getScraperError(),
-        	'magnitude' => $magnitude,
-        ));
+
+	    ### Render ####################################################################
+	    $response = $this->render("JlamHorkosBundle:Horkos:index.$format.twig", array(
+    				'ridings'	=> Riding::getAllRdingsSorted(),
+    				'partyTally' 	=> $partyTally,
+    				'jurisdiction'	=> $jurisdictionTally,
+    				'summary'	=> $summary,
+    				'election'	=> $election ? $election : self::DEFAULT_ELECTION,
+    				'error'		=> $engineClassName::getScraperError(),
+    				'magnitude' => $magnitude,
+    		));
 
 
-        
-        
+
+
         #Save into caching
         $date = new \DateTime();
         $date->modify('+'. self::CACHE_TTL_SECS .' seconds');
-        
+
         $response->setPublic();
         $response->setExpires($date);
         $response->setMaxAge(self::CACHE_TTL_SECS);
         $response->setSharedMaxAge(self::CACHE_TTL_SECS);
-        
+
 		$cache->set($cacheKey, $response, self::CACHE_TTL_SECS);
-                
-        
-		#Return the controller 
+
+
+		#Return the controller
         return $response;
     }
-    
-    
+
+
     private static function getScrappingEngineClassName($electionShorthand = null) {
     	$engineClassNames = array(
     		'cdn2015'	=> 'Cdn2015scrapper',
     		'ab2015'	=> 'Ab2015scrapper',
     	);
-    	
-    	if(!isset($engineClassNames[$electionShorthand])) 
+
+    	if(!isset($engineClassNames[$electionShorthand]))
     		$electionShorthand = self::DEFAULT_ELECTION;
-    	
+
     	$engineClassName =  $engineClassNames[$electionShorthand];
-    	
+
     	return self::BASE_DIR_SCRAPPERS . $engineClassName;
     }
 }
