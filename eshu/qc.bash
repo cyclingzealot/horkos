82d5df848d412c13558237109ddd8f0c0554f59d
Julien Lamarche <julien.lamarche@gmail.com>
2017-04-13 21:45:11 -0400

    added  qc.bash

---
+++ eshu/qc.bash
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+
+START=$(date +%s.%N)
+
+#exit when command fails (use || true when a command can fail)
+set -o errexit
+
+#exit when your script tries to use undeclared variables
+set -o nounset
+
+#(a.k.a set -x) to trace what gets executed
+#set -o xtrace
+
+# in scripts to catch mysqldump fails
+set -o pipefail
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file})"
+ts=`date +'%Y%m%d-%H%M%S'`
+
+#Set the config file
+configFile="$HOME/.binJlam/templateConfig"
+
+#Ensure only one copy is running
+pidfile=$HOME/.${__base}.pid
+if [ -f ${pidfile} ]; then
+   #verify if the process is actually still running under this pid
+   oldpid=`cat ${pidfile}`
+   result=`ps -ef | grep ${oldpid} | grep ${__base}  || true`
+
+   if [ -n "${result}" ]; then
+     echo "Script already running! Check $pidfile. Exiting"
+     exit 255
+   fi
+fi
+
+#grab pid of this process and update the pid file with it
+pid=`ps -ef | grep ${__base} | grep -v 'vi ' |  head -n1 |  awk ' {print $2;} '`
+echo ${pid} > ${pidfile}
+
+
+#Capture everything to log
+mkdir -p ~/log
+log=~/log/$__base-${ts}.log
+exec >  >(tee -a $log)
+exec 2> >(tee -a $log >&2)
+touch $log
+chmod 600 $log
+
+
+#Check that the config file exists
+#if [[ ! -f "$configFile" ]] ; then
+#        echo "I need a file at $configFile with ..."
+#        exit 1
+#fi
+
+
+echo Begin `date`  .....
+
+### BEGIN SCRIPT ###############################################################
+
+
+
+electionID=`echo $__base | cut -d '.' -f 1`
+curlTimeout=6
+restSecs=10
+continueFlag=/tmp/$__base.continue
+stopFlag=/tmp/$__base.stop
+maxRuns=100
+
+touch $continueFlag
+
+if [[ -f $stopFlag ]]; then
+	echo Stop Flag active.  To remove
+	echo rm $stopFlag
+	exit 1
+fi
+
+runs=0
+set -x
+while [[ -f $continueFlag && $runs -lt $maxRuns && ! -f $stopFlag  ]]; do
+set +x
+	echo Run $runs of $maxRuns...
+
+	for lang in f ; do
+		dataDir="$__dir/data/$electionID/$lang/"
+		workDir="$dataDir/work/"
+		readyDir="$dataDir/ready"
+        jsonSource='http://dgeq.org/resultats.json'
+        minChars=10000
+
+		mkdir -p $workDir $readyDir
+
+		ridingFile=$workDir/resultats.json
+		readyFile=$readyDir/resultats.json
+
+		startCurl=$(date +%s.%N)
+		curl -m $curlTimeout -s $jsonSource > $ridingFile || true
+        #dos2unix $ridingFile
+		test `wc -c $ridingFile  | cut -f 1 -d' '` -gt $minChars && mv -v "$ridingFile" "$readyFile" || echo "$ridingFile has less than $minLines"
+		endCurl=$(date +%s.%N)
+		diffCurl=$(echo "$endCurl - $startCurl" | bc)
+
+		echo; echo
+		echo Data for file $jsonSource in language $lang done!
+		echo; echo
+	done
+
+	date
+	echo Resting for $restSecs seconds....
+	echo
+	echo To interupt until next cron:
+	echo rm $continueFlag
+	echo
+	echo To stop permantly
+	echo touch $stopFlag
+	echo
+	for i in `seq $restSecs -1 0`; do
+		printf "$i... " ;
+		if (( $i % 5 == 0 )) ; then echo ; fi
+		sleep 1;
+	done
+	echo ; echo
+
+	echo Run $runs of $maxRuns done
+
+	let runs++ || true
+
+
+done
+
+
+echo; echo; echo; echo;
+
+
+
+### END SCIPT ##################################################################
+
+END=$(date +%s.%N)
+DIFF=$(echo "$END - $START" | bc)
+echo Done.  `date` - $DIFF seconds
+
+if [ -f ${pidfile} ]; then
+    rm ${pidfile}
+fi
