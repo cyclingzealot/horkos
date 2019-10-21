fe732923103b77e08f83e36445516a9c89f3ce7e
Julien Lamarche <julien.lamarche@gmail.com>
2019-10-20 23:20:27 -0400

    changed election handle from 2015 to 2019, changed contact info

--- symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
+++ symfony/src/Jlam/HorkosBundle/Controller/HorkosController.php
@@ -14,7 +14,7 @@ class HorkosController extends Controller
 {
 
 	const BASE_DIR_SCRAPPERS	= 'Jlam\HorkosBundle\\';
-	const DEFAULT_ELECTION		= 'ab2019';
+	const DEFAULT_ELECTION		= 'cdn2019';
 	const CACHE_TTL_SECS		= 30;
 
     public function indexAction()
@@ -140,7 +140,7 @@ class HorkosController extends Controller
 
     public static function getElectionNames() {
         return array(
-    		'cdn2015'	=> 'Canadian 2015 election',
+    		'cdn2019'	=> 'Canadian 2015 election',
     		'sk2016'	=> 'Saskatchewan 2016 election',
     		'on2018'	=> 'Ontario 2018 election',
     		'ab2019'	=> 'Alberta 2019 election',
@@ -175,7 +175,7 @@ class HorkosController extends Controller
     }
     public static function getScrappingEngineClassName($electionShorthand = null) {
         $engineClassNames = array(
-            'cdn2015'   => 'Cdn2015scrapper',
+            'cdn2019'   => 'Cdn2015scrapper',
             'ab2019'    => 'Ab2015scrapper',
             'sk2016'    => 'Sk2016scrapper',
             'on2018'    => 'On2018scrapper',
